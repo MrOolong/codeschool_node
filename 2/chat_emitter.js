a0746424414beba4db6008fc0100851bab0133f8
Dennis Daubney Jr <dennis.daubney@gmail.com>
2017-09-09 18:31:15 +0000

    Emitting events

---
+++ 2/chat_emitter.js
@@ -0,0 +1,18 @@
+//Emitting events
+var events = require('events');
+var EventEmitter = events.EventEmitter;
+
+var chat = new EventEmitter();
+var users = [], chatlog = [];
+
+chat.on('message', function(message) {
+  chatlog.push(message);
+});
+
+chat.on('join', function(nickname) {
+  users.push(nickname);
+});
+
+// Emit events here
+chat.emit('join', "cheese rules!");
+chat.emit('message', "Mozzarella!");
