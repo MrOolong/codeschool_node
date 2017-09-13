8509b9be2aa4dde71b93ff8da08a00afc3eb2b71
Dennis Daubney Jr <dennis.daubney@gmail.com>
2017-09-13 21:58:30 +0000

    updated comments

--- 4/app.js
+++ 4/app.js
@@ -4,3 +4,20 @@ myMod.bar();
 
 var http = require("http");
 var message = "Here's looking at you, kid.";
+
+var makeRequest = function(message) {
+    var options = {
+      host: 'localhost', port: 8080, path: '/', method: 'POST'  
+    };
+    
+    var request = http.request(options, function(response){
+       response.on('data', function(data){
+           console.log(data); //logs reponse body
+       }); 
+    });
+
+request.write(message); //begins request
+request.end(); //ends request
+}
+
+makeRequest("Here's looking at you kid.") //call function with string
